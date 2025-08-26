5dc5a1ecf4a0ace5e65ae301fe5097fb18922b11
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2025-08-26 19:26:25 +0100

    base project setup

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Date       : 26/08/2025
+// Project    : tm_parse
+// Author     : -Ry
+//
+
+#include "tm_parse/test.h"
+
+int main() { tm_parse::Test{}.hello(); }
---
+++ src/tm_parse/test.h
@@ -0,0 +1,16 @@
+//
+// Date       : 26/08/2025
+// Project    : tm_parse
+// Author     : -Ry
+//
+
+#pragma once
+
+namespace tm_parse {
+
+class Test {
+public:
+  void hello();
+};
+
+} // namespace tm_parse
