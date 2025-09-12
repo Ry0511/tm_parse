b44b76647d9e33365b8cee43e300a8ad028c4f04
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2025-09-13 00:06:23 +0100

    dont colour code function name and shorten anonymous namespaces when logging

--- src/tm_parse/util/logging.cpp
+++ src/tm_parse/util/logging.cpp
@@ -15,10 +15,10 @@ std::string_view get_log_level_name(LogLevel level) {
     // clang-format off
     switch (level) {
         case LogLevel::Trace: return "\033[32mTRACE\033[0m"; // Green
-        case LogLevel::Info:  return "\033[32mINFO \033[0m"; // Green
-        case LogLevel::Warn:  return "\033[33mWARN \033[0m"; // Yellow
+        case LogLevel::Info:  return "\033[32m INFO\033[0m"; // Green
+        case LogLevel::Warn:  return "\033[33m WARN\033[0m"; // Yellow
         case LogLevel::Err:   return "\033[31mERROR\033[0m"; // Red
-        default:              return "\033[34mIDK  \033[0m"; // Blue
+        default:              return "\033[34m  IDK\033[0m"; // Blue
     }
     // clang-format on
 }
@@ -46,32 +46,42 @@ std::string_view get_file_name(const std::source_location& src) {
     return std::string_view{start, end};
 }
 
-std::string_view get_function_name(const std::source_location& src) {
-    // Just grab the actual function name since it logs the full function
-    // i.e.,
-    //   _cdecl void my_namespace::my_function(const int&)
+std::string get_function_name(const std::source_location& src) {
     const char* func = src.function_name();
     const char* start = func;
     const char* end = func;
 
     // clang-format off
-    while (*end != '(')                    { ++end;   } start = end;
-    while (*start != ':' && *start != ' ') { --start; } ++start;
+    while (*end != '(')   { ++end;   } start = end;
+    while (*start != ' ') { --start; } ++start;
     // clang-format on
 
-    return std::string_view{start, end};
+    std::string_view func_view{start, end};
+    std::string func_str{};
+    func_str.reserve(func_view.size());
+
+    for (size_t i = 0; i < func_view.size(); ++i) {
+        if (func_view[i] == '`') {
+            func_str += "`anon':";
+            i += sizeof("`anonymous-namespace`") - 1;
+        } else {
+            func_str += func_view[i];
+        }
+    }
+
+    return func_str;
 }
 
 }  // namespace
 
 void log(LogLevel level, std::string_view msg, const std::source_location& src) {
     std::string_view file_name = get_file_name(src);
-    std::string_view function_name = get_function_name(src);
+    std::string function_name = get_function_name(src);
 
     std::string header = std::format(
         "[{:>5}] {:>60}:{:<3}",
         get_log_level_name(level),
-        truncate_left(std::format("\033[34m{}\033[0m {}", function_name, file_name), 60),
+        truncate_left(std::format("{} {}", function_name, file_name), 60),
         src.line()
     );
 
